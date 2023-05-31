c4c8ac5b6f71665193920d8b2fa343a53671ba49
Harsh Gupta <59175412+harshgupta20@users.noreply.github.com>
2023-05-31 12:35:53 +0530

    Config file

--- src/config/Appwrite.js
+++ src/config/Appwrite.js
@@ -2,7 +2,7 @@ import {Client, Account, Databases} from "appwrite";
 
 const client = new Client();
 
-client.setEndpoint("https://cloud.appwrite.io/v1").setProject("645f7b3c5f2eb7d8b387");
+client.setEndpoint("https://cloud.appwrite.io/v1").setProject("PROJECT_ID");
 
 export const account = new Account(client);
-export const database = new Databases(client, "645f813d27ffb304f3c2");
+export const database = new Databases(client, "DATABASE_ID");
